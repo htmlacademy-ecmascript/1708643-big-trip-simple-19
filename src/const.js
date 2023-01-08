f0137bc442373ac161e4dc8f3ff005b3d50fda42
Margo <mnchek@mail.ru>
2023-01-08 22:24:15 +0300

    время разбивать камни

---
+++ src/const.js
@@ -0,0 +1,3 @@
+const POINT_LIST_RENDER_COUNT = 3;
+
+export { POINT_LIST_RENDER_COUNT };
---
+++ src/presenter/trip-presenter.js
@@ -0,0 +1,27 @@
+import {render} from '../render.js';
+import PointEditView from '../view/point-edit-view.js';
+import FilterView from '../view/filter-view.js';
+import PointView from '../view/point-view.js';
+import SortView from '../view/sort-view.js';
+import PointListView from '../view/point-list-view.js';
+import POINT_LIST_RENDER_COUNT from '../const.js';
+
+export default class TripPresenter {
+  pointListView = new PointListView();
+
+  constructor({filterContainer, siteMainContainer}) {
+    this.filterContainer = filterContainer;
+    this.siteMainContainer = siteMainContainer;
+  }
+
+  init() {
+    render(new PointEditView(), this.pointListView.getElement());
+    render(new FilterView(), this.filterContainer);
+    render(new SortView(), this.siteMainContainer);
+    render(this.pointListView, this.siteMainContainer);
+
+    for (let i = 0; i < POINT_LIST_RENDER_COUNT; i++) {
+      render(new PointView(), this.pointListView.getElement());
+    }
+  }
+}
