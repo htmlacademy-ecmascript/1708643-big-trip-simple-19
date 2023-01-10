43a9bb1a41dab1d20e92ebfc75d387eda5aafcae
Margo <mnchek@mail.ru>
2023-01-10 22:23:05 +0300

    время разбивать камни правка

--- src/presenter/trip-presenter.js
+++ src/presenter/trip-presenter.js
@@ -4,7 +4,7 @@ import FilterView from '../view/filter-view.js';
 import PointView from '../view/point-view.js';
 import SortView from '../view/sort-view.js';
 import PointListView from '../view/point-list-view.js';
-import POINT_LIST_RENDER_COUNT from '../const.js';
+import { POINT_LIST_RENDER_COUNT } from '../const.js';
 
 export default class TripPresenter {
   pointListView = new PointListView();
